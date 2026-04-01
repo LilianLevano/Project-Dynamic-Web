b63d9e47156a84b0e6fd538438cfa1c7ec3648e2
benbilal94 <benhambilal94@gmail.com>
2026-04-01 11:20:13 +0200

    filer aangemaakt

--- brussels-explorer/js/index.js
+++ brussels-explorer/js/index.js
@@ -1,26 +1,40 @@
 "use strict";
 
 const fetchData = async () => {
-    try {
-        const res = await fetch("https://opendata.brussels.be/api/explore/v2.1/catalog/datasets/bruxelles_lieux_culturels/records?limit=20")
-           if (!res.ok) {
-            throw new Error("could not fetch")
-           }
-           const data = await res.json()
-           console.log(data);
-           return data;
-           
-           
-    } catch (error) {
-        
+  try {
+    const res = await fetch(
+      "https://opendata.brussels.be/api/explore/v2.1/catalog/datasets/bruxelles_lieux_culturels/records?limit=20",
+    );
+    if (!res.ok) {
+      throw new Error("could not fetch");
     }
-}
-
-document.addEventListener('DOMContentLoaded', async ()=>{
-    
-    const data = await fetchData();
+    const data = await res.json();
     console.log(data);
-    
+    return data;
+  } catch (error) {}
+};
+
+let inputFiltervalue = document.getElementById("input-filter-postcode").value;
+
+inputFiltervalue = Number(inputFiltervalue);
+
+const filterbutton = document.getElementById("filter");
 
 
-})
+filterbutton.addEventListener("click", async () => {
+
+  const data = await fetchData();
+  
+  data.results.forEach((element) => {
+
+    if (inputFiltervalue === element.code_postal) {
+      console.log(element);
+    }
+  });
+
+});
+
+document.addEventListener("DOMContentLoaded", async () => {
+  const data = await fetchData();
+  console.log(data);
+});
