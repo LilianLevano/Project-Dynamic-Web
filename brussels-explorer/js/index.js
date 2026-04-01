73bb0bf9f37cc2c3885645c74589c6ab2bf35fb3
Lilian Levano <zey.lilianlevano@gmail.com>
2026-04-01 11:20:04 +0200

    updateMainList aangemaakt

--- brussels-explorer/js/index.js
+++ brussels-explorer/js/index.js
@@ -16,10 +16,88 @@ const fetchData = async () => {
     }
 }
 
+const updateMainList = (arrayResults) =>{
+    const sectionCardContainer = document.getElementById('table-container')
+    sectionCardContainer.innerHTML = "";
+
+    for(let locatie of arrayResults){
+
+
+        const lat = locatie.coordonnees_geographiques.lat;
+        const lon = locatie.coordonnees_geographiques.lon;
+
+        const card = document.createElement('article')
+        card.classList.add('card')
+
+
+
+        const iframe = document.createElement('iframe')
+        iframe.src = `https://www.google.com/maps?q=&layer=c&cbll=${lat},${lon}&cbp=11,0,0,0,0&output=svembed`
+
+            iframe.setAttribute("allow", "accelerometer; gyroscope");
+            iframe.setAttribute("allowfullscreen", "true");
+
+            iframe.width = "450";
+            iframe.height = "350";
+            iframe.style.border = "0";
+
+        card.appendChild(iframe)
+
+
+
+        const divInformatieText = document.createElement('div')
+        divInformatieText.classList.add('informatie-text')
+
+        
+        const beschrijvingLocatie = document.createElement('h2')
+        beschrijvingLocatie.textContent = locatie.beschrijving 
+        divInformatieText.appendChild(beschrijvingLocatie)
+
+        const adres = document.createElement('p')
+        adres.textContent = locatie.adres + ", "
+        divInformatieText.appendChild(adres)
+
+        const postCode = document.createElement('span')
+        postCode.textContent = locatie.code_postal
+        adres.appendChild(postCode)
+
+        const plaats = document.createElement('p')
+        plaats.textContent = locatie.plaats
+        divInformatieText.appendChild(plaats)
+
+        const geolocatie = document.createElement('p')
+        geolocatie.textContent = locatie.coordonnees_geographiques.lat + ", " + locatie.coordonnees_geographiques.lat
+        divInformatieText.appendChild(geolocatie)
+
+        card.appendChild(divInformatieText)
+
+        const iframeMaps = document.createElement('iframe')
+        iframeMaps.src = `https://www.google.com/maps?q=${lat},${lon}&output=embed`
+        
+                    iframeMaps.setAttribute("allowfullscreen", "true");
+                    iframeMaps.setAttribute("lazy", "true");
+
+            iframeMaps.width = "300";
+            iframeMaps.height = "250";
+            iframeMaps.style.border = "0";
+        
+        card.appendChild(iframeMaps)
+
+        sectionCardContainer.appendChild(card)
+
+    }
+}
+
 document.addEventListener('DOMContentLoaded', async ()=>{
     
     const data = await fetchData();
-    console.log(data);
+    const arrayResults = data.results 
+
+    updateMainList(arrayResults);
+
+    
+    
+  
     
 
 
