8dcf3c31934e5bb1dca3febff66d5e2367eb5c4d
benbilal94 <benhambilal94@gmail.com>
2026-04-01 10:45:19 +0200

    js

--- brussels-explorer/js/index.js
+++ brussels-explorer/js/index.js
@@ -1,17 +1,16 @@
-'use strict';
+"use strict";
 
 const fetchData = async () => {
-    try {
-        const res = await fetch("https://opendata.brussels.be/api/explore/v2.1/catalog/datasets/bruxelles_lieux_culturels/records?limit=20")
-           if (!res.ok) {
-            throw new Error("could not fetch")
-           }
-           const data = await res.json()
-           console.log(data);
-           
-    } catch (error) {
-        
+  try {
+    const res = await fetch(
+      "https://opendata.brussels.be/api/explore/v2.1/catalog/datasets/bruxelles_lieux_culturels/records?limit=20",
+    );
+    if (!res.ok) {
+      throw new Error("could not fetch");
     }
-}
+    const data = await res.json();
+    console.log(data);
+  } catch (error) {}
+};
 
-fetchData()
+fetchData();
